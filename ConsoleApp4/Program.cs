e12f284001207f53b0565f353b6f24fa4c332fc0
Aytac <111279845+Seniorita97@users.noreply.github.com>
2024-03-07 13:34:24 +0400

    task finished

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int a = 5;
+
+//for (int i = 1;i <= a; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
+
+//-----------------------------------------------------------------------------------
+
+//int[] ciArr1 = { 211, 9, -6, 12, 34, 56, 32, 5, 19 };
+//int[] ciArr2 = { 19, 6, 32 };
+//bool check = true;
+//for (int i = 0; i < ciArr2.Length; i++)
+//{
+//    bool found = false;
+//    for (int j = 0; j < ciArr1.Length; j++)
+//    {
+//        if (ciArr2[i] == ciArr1[j])
+//        {
+//            found = true;
+//            break;
+//        }
+//    }
+//    if (!found)
+//    {
+//        check = false;
+//        break;
+//    }
+//}
+//Console.WriteLine(check);
+
+
+
